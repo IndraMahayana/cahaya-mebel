a605282cd385d022d516d3541c7d522317be236a
Jas <hacyonukumori@gmail.com>
2022-08-30 18:16:50 +0800

    cahaya mebel

---
+++ index.js
@@ -0,0 +1,21 @@
+const navbar = document.querySelector('.navbar')
+const upButton = document.querySelector('.up-button')
+const navbarUl1 = document.querySelector('.navbar-ul-1')
+const navbarUl2 = document.querySelector('.navbar-ul-2')
+const navbarUl3 = document.querySelector('.navbar-ul-3')
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navbar.classList.add('navbar-change-color')
+        upButton.style.visibility = 'visible'
+        navbarUl1.style.color = 'white'
+        navbarUl2.style.color = 'white'
+        navbarUl3.style.color = 'white'
+    } else {
+        navbar.classList.remove('navbar-change-color')
+        upButton.style.visibility = 'hidden'
+        navbarUl1.style.color = '#333'
+        navbarUl2.style.color = '#333'
+        navbarUl3.style.color = '#333'
+    }
+}
